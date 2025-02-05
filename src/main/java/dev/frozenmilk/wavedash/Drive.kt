77c8ac75c24881a54dcc4c31c9445db17603a4f9
Zach Harel <zach.harel42@gmail.com>
2025-02-05 18:41:22 -0500

    use lambdas like regular people

--- src/main/java/dev/frozenmilk/wavedash/Drive.kt
+++ src/main/java/dev/frozenmilk/wavedash/Drive.kt
@@ -7,6 +7,7 @@ import com.acmerobotics.roadrunner.TimeTurn
 import com.acmerobotics.roadrunner.Vector2d
 import dev.frozenmilk.dairy.core.wrapper.Wrapper
 import dev.frozenmilk.mercurial.commands.Command
+import dev.frozenmilk.mercurial.commands.Lambda
 
 interface Drive {
     val localizer: Localizer
@@ -21,70 +22,40 @@ interface Drive {
 
     fun followTrajectoryCommand(trajectory: TimeTrajectory): Command {
         val requirements = setOf(this)
-
         val states: Set<Wrapper.OpModeState> = setOf(Wrapper.OpModeState.ACTIVE)
+        val start = System.nanoTime() * 1e-9
+        var t = 0.0
+        var finished = false
 
-        return object : Command {
-            var t: Double = 0.0
-            val beginTs: Double = System.nanoTime() * 1e-9
-
-            var finished: Boolean = false
-
-            override val requirements = requirements
-
-            override val runStates = states
-
-            override fun toString() = "FollowTrajectoryCommand"
-
-            override fun initialise() {}
-
-            override fun execute() {
-                t = (System.nanoTime() * 1e-9) - beginTs
-                finished = followTrajectory(trajectory, t)
+        return Lambda("Follow Trajectory Command")
+            .setRequirements(requirements)
+            .setRunStates(states)
+            .setExecute {
+                finished = this.followTrajectory(trajectory, t)
+                t = start - System.nanoTime()
+            }.setFinish { finished }
+            .setEnd {
+                this.setDrivePowers(PoseVelocity2d(Vector2d(0.0, 0.0), 0.0))
             }
-
-            override fun finished(): Boolean {
-                return finished
-            }
-
-            override fun end(interrupted: Boolean) {
-                setDrivePowers(PoseVelocity2d(Vector2d(0.0, 0.0), 0.0))
-            }
-        }
     }
 
     fun turnCommand(turn: TimeTurn): Command {
         val requirements = setOf(this)
-
         val states: Set<Wrapper.OpModeState> = setOf(Wrapper.OpModeState.ACTIVE)
+        val start = System.nanoTime() * 1e-9
+        var t = 0.0
+        var finished = false
 
-        return object : Command {
-            var t: Double = 0.0
-            val beginTs: Double = System.nanoTime() * 1e-9
-
-            var finished: Boolean = false
-
-            override val requirements = requirements
-
-            override val runStates = states
-
-            override fun toString() = "TurnCommand"
-
-            override fun initialise() {}
-
-            override fun execute() {
-                t = (System.nanoTime() * 1e-9) - beginTs
-                finished = turn(turn, t)
+        return Lambda("Turn Command")
+            .setRequirements(requirements)
+            .setRunStates(states)
+            .setExecute {
+                finished = this.turn(turn, t)
+                t = start - System.nanoTime()
+            }.setFinish { finished }
+            .setEnd {
+                this.setDrivePowers(PoseVelocity2d(Vector2d(0.0, 0.0), 0.0))
             }
-
-            override fun finished(): Boolean {
-                return finished
-            }
-
-            override fun end(interrupted: Boolean) {
-                setDrivePowers(PoseVelocity2d(Vector2d(0.0, 0.0), 0.0))
-            }
-        }
     }
 }
 
